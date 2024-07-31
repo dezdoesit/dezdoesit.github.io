cccf040966b7c2d311e84d0c89f9ecfe160d3b0d
dezdoesit <dez.blair06@gmail.com>
2024-07-31 13:50:19 -0400

    Ignite setup for Github Pages

---
+++ dezdoesit/Sources/Site.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Ignite
+
+@main
+struct IgniteWebsite {
+    static func main() async {
+        let site = ExampleSite()
+
+        do {
+            try await site.publish()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct ExampleSite: Site {
+
+    var name = "Hello World"
+    var titleSuffix = " – My Awesome Site"
+    var url = URL("dezdoesit.github.io")
+    var builtInIconsEnabled = true
+
+    var author = "John Appleseed"
+
+    var homePage = Home()
+    var theme = MyTheme()
+}
+
+
